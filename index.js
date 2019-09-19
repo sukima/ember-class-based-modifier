ee02ed70f6590c947e12e75fa907cd32b07e0bce
Devin Weaver <suki@tritarget.org>
2019-09-19 10:21:14 -0400

    Deprecate ember-oo-modifiers
    This has been renamed to ember-class-based-modifier

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
+const chalk = require('chalk');
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included: function() {
+    this.ui.writeLine(chalk.yellow('DEPRECATION: ember-oo-modifiers has been renamed to ember-class-based-modifier. Updates will continue there.'));
+    return this._super.included.call(this, ...arguments);
+  }
 };
