dffb670f05eebe2e5ae71aa0dfb5977587b30769
Godfrey Chan <godfreykfc@gmail.com>
2019-09-18 20:59:06 -0700

    Squashed commits from ember-class-based-modifier
    This is a result of squashing everything after the v0.5.0 tag up to and
    including the v0.9.0 tag, rebased on top of the current master, which is
    the v0.5.2 tag.
    
    The original commits are at the [archive/ember-class-based-modifier-fork
    branch](https://github.com/sukima/ember-oo-modifiers/tree/archive/ember-class-based-modifier-fork)
    as a permanent archive, but the commits themselves are not very
    interesting. It's mainly there so that the `v0.8.0` and `v0.9.0` tags
    have something to point to.

---
+++ tests/integration/modifier-managers/native-test.js
@@ -0,0 +1,75 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import Service, { inject as service } from '@ember/service';
+import hbs from 'htmlbars-inline-precompile';
+import Modifier from 'ember-class-based-modifier';
+import { testHooks } from './shared';
+
+module('Integration | Modifier Manager | class-based modifier (native class)', function(hooks) {
+  setupRenderingTest(hooks);
+
+  testHooks(callback => class NativeModifier extends Modifier {
+    constructor() {
+      super(...arguments);
+      callback('constructor', this);
+    }
+
+    didReceiveArguments() {
+      callback('didReceiveArguments', this);
+    }
+
+    didUpdateArguments() {
+      callback('didUpdateArguments', this);
+    }
+
+    didInstall() {
+      callback('didInstall', this);
+    }
+
+    willRemove() {
+      callback('willRemove', this);
+    }
+
+    willDestroy() {
+      callback('willDestroy', this);
+    }
+  });
+
+  module('service injection', function() {
+    test('can participate in ember dependency injection', async function(assert) {
+      let called = false;
+
+      this.owner.register(
+        'service:foo',
+        Service.extend({ isFooService: true })
+      );
+
+      this.owner.register(
+        'service:bar',
+        Service.extend({ isBarService: true })
+      );
+
+      this.owner.register(
+        'modifier:songbird',
+        class NativeModifier extends Modifier {
+          @service foo;
+          @service('bar') baz;
+
+          constructor() {
+            super(...arguments);
+
+            called = true;
+
+            assert.strictEqual(this.foo.isFooService, true, 'this.foo.isFooService');
+            assert.strictEqual(this.baz.isBarService, true, 'this.baz.isBarService');
+          }
+        }
+      );
+
+      await render(hbs`<h1 {{songbird}}>Hello</h1>`);
+
+      assert.strictEqual(called, true, 'constructor called');
+    });
+  });
+});
